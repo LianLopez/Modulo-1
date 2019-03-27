b89db3196636960d457ffd3b04193a15771cf8ca
Leonardo Lopez <lopez.leonardo.info@gmail.com>
2019-03-27 12:24:14 -0300

    jQuery effects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,69 @@
 $("#menu-jquery").click(function() {
   $(".nav3").slideToggle("slow").css("display", "flex");
 });
+
+
+$(".button1-map").click(function() {
+  $(".maps-none1").slideDown("slow");
+});
+$(".button-closemap1").click(function() {
+  $(".maps-none1").slideUp("slow");
+});
+$(".maps-none1").click(function() {
+  $(".maps-none1").slideUp("slow");
+});
+
+
+$(".button2-map").click(function() {
+  $(".maps-none2").slideDown("slow");
+});
+$(".button-closemap2").click(function() {
+  $(".maps-none2").slideUp("slow");
+});
+$(".maps-none2").click(function() {
+  $(".maps-none2").slideUp("slow");
+});
+
+
+$(".button3-map").click(function() {
+  $(".maps-none3").slideDown("slow");
+});
+$(".button-closemap3").click(function() {
+  $(".maps-none3").slideUp("slow");
+});
+$(".maps-none3").click(function() {
+  $(".maps-none3").slideUp("slow");
+});
+
+
+$(".button4-map").click(function() {
+  $(".maps-none4").slideDown("slow");
+});
+$(".button-closemap4").click(function() {
+  $(".maps-none4").slideUp("slow");
+});
+$(".maps-none4").click(function() {
+  $(".maps-none4").slideUp("slow");
+});
+
+
+$(".button5-map").click(function() {
+  $(".maps-none5").slideDown("slow");
+});
+$(".button-closemap5").click(function() {
+  $(".maps-none5").slideUp("slow");
+});
+$(".maps-none5").click(function() {
+  $(".maps-none5").slideUp("slow");
+});
+
+
+$(".button6-map").click(function() {
+  $(".maps-none6").slideDown("slow");
+});
+$(".button-closemap6").click(function() {
+  $(".maps-none6").slideUp("slow");
+});
+$(".maps-none6").click(function() {
+  $(".maps-none6").slideUp("slow");
+});
